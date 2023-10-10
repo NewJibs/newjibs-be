66a282f1a2c182625504fe53235e7090efbc1a68
takealook <takealook97@naver.com>
2023-10-10 17:20:43 +0900

    feat: add Notice Service class
    add basic CRUD methods

---
+++ src/main/java/com/ssafy/newjibs/notice/service/NoticeService.java
@@ -0,0 +1,48 @@
+package com.ssafy.newjibs.notice.service;
+
+import com.ssafy.newjibs.notice.domain.Notice;
+import com.ssafy.newjibs.notice.dto.NoticeDto;
+import com.ssafy.newjibs.notice.dto.NoticeListDto;
+import com.ssafy.newjibs.notice.repository.NoticeRepository;
+import com.ssafy.newjibs.notice.util.NoticeMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NoticeService {
+    private final NoticeRepository noticeRepository;
+    private final NoticeMapper noticeMapper;
+
+    public void createNotice(NoticeDto noticeDto) {
+        Notice notice = noticeMapper.toEntity(noticeDto);
+        noticeRepository.save(notice);
+    }
+
+    public NoticeDto readNotice(Long noticeId) {
+        Notice notice = noticeRepository.findByNoticeId(noticeId).orElseThrow(RuntimeException::new);// exception
+        return noticeMapper.toDto(notice);
+    }
+
+    public List<NoticeListDto> readAllNotice() {
+        return noticeRepository.findAll()
+                .stream()
+                .map(noticeMapper::toListDto)
+                .collect(Collectors.toList());
+    }
+
+    public void updateNotice(Long noticeId, NoticeDto noticeDto) {
+        Notice notice = noticeRepository.findByNoticeId(noticeId).orElseThrow(RuntimeException::new);// exception
+        notice.setTitle(noticeDto.getTitle());
+        notice.setContent(notice.getContent());// dirty checking
+    }
+
+    public void deleteNotice(Long noticeId) {
+        noticeRepository.delete(noticeRepository.findByNoticeId(noticeId).orElseThrow(RuntimeException::new));// exception
+    }
+}
