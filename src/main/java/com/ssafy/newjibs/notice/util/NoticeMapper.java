be855e6937987bb64041c8fcc27ad2b5fda6c6b3
takealook <takealook97@naver.com>
2023-10-10 17:19:08 +0900

    feat: add Notice Mapper class
    add toEntity, toDto, toListDto methods

---
+++ src/main/java/com/ssafy/newjibs/notice/util/NoticeMapper.java
@@ -0,0 +1,37 @@
+package com.ssafy.newjibs.notice.util;
+
+import com.ssafy.newjibs.notice.domain.Notice;
+import com.ssafy.newjibs.notice.dto.NoticeDto;
+import com.ssafy.newjibs.notice.dto.NoticeListDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NoticeMapper {
+    public Notice toEntity(NoticeDto noticeDto) {
+        return Notice.builder()
+                .title(noticeDto.getTitle())
+                .author(noticeDto.getAuthor())
+                .content(noticeDto.getContent())
+                .imgUrl(noticeDto.getImgUrl())
+                .date(noticeDto.getDate())
+                .build();
+    }
+
+    public NoticeDto toDto(Notice notice) {
+        return NoticeDto.builder()
+                .title(notice.getTitle())
+                .author(notice.getAuthor())
+                .content(notice.getContent())
+                .imgUrl(notice.getImgUrl())
+                .date(notice.getDate())
+                .build();
+    }
+
+    public NoticeListDto toListDto(Notice notice) {
+        return NoticeListDto.builder()
+                .title(notice.getTitle())
+                .author(notice.getAuthor())
+                .date(notice.getDate())
+                .build();
+    }
+}
