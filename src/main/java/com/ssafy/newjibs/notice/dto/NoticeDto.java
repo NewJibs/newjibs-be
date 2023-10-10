289d0e5fe78bca1e9b2fb4c1c3aa3d16003d0492
takealook <takealook97@naver.com>
2023-10-10 17:18:29 +0900

    feat: add Notice Dto class

---
+++ src/main/java/com/ssafy/newjibs/notice/dto/NoticeDto.java
@@ -0,0 +1,32 @@
+package com.ssafy.newjibs.notice.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeDto {
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("author")
+    private String author;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("imgUrl")
+    private String imgUrl;
+
+    @JsonProperty("date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime date;
+}
