380bf291e9b0a80611b7f9f1270578d54f3dcf18
takealook <takealook97@naver.com>
2023-10-10 10:22:12 +0900

    build: init

---
+++ src/main/java/com/ssafy/newjibs/NewjibsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.newjibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewjibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewjibsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/newjibs/NewjibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.newjibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewjibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
