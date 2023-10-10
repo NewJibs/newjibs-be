5336902b15d6d6d000bcc60cbde084f681a36f1b
takealook <takealook97@naver.com>
2023-10-10 17:18:05 +0900

    feat: add Notice Entity class

---
+++ src/main/java/com/ssafy/newjibs/notice/domain/Notice.java
@@ -0,0 +1,38 @@
+package com.ssafy.newjibs.notice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "notice")
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noticeId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private String imgUrl;
+
+    @Column(nullable = false, columnDefinition = "DATETIME(0)")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime date;
+}
