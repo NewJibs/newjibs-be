632aff80e2afc4f35869719121be7a55f43f87b9
takealook97 <takealook97@naver.com>
2023-10-12 05:28:13 +0900

    feat: add News Repository interface
    put find methods to read and add exist checking method to prevent double check

---
+++ src/main/java/com/ssafy/newjibs/news/repository/NewsRepository.java
@@ -0,0 +1,16 @@
+package com.ssafy.newjibs.news.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.newjibs.news.domain.News;
+
+public interface NewsRepository extends JpaRepository<News, Long> {
+	Optional<News> findByArticleId(String articleId);
+
+	List<News> findAll();
+
+	Boolean existsByArticleId(String articleId);
+}
