e3a3df3f0779cdbd630d19bddb1f5a3254312143
takealook <takealook97@naver.com>
2023-10-12 17:27:47 +0900

    feat: add Dongcode entity class

---
+++ src/main/java/com/ssafy/newjibs/house/detail/dongcode/domain/DongCode.java
@@ -0,0 +1,29 @@
+package com.ssafy.newjibs.house.detail.dongcode.domain;
+
+import com.ssafy.newjibs.house.detail.houseinfo.domain.HouseInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "dongcode")
+public class DongCode {
+    @Id
+    @Column(nullable = false)
+    private String dongCode;// use api inner id as a pk
+    private String sidoName;
+    private String gugunName;
+    private String dongName;
+
+    @OneToMany(mappedBy = "dongcode", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HouseInfo> houseInfos;
+
+}
