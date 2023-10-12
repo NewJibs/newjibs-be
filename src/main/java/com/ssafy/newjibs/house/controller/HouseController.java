6285e8575c349ebc8b7afbf0b20b5f6bcf2ead33
takealook <takealook97@naver.com>
2023-10-12 17:36:25 +0900

    feat: add House controller class

---
+++ src/main/java/com/ssafy/newjibs/house/controller/HouseController.java
@@ -0,0 +1,14 @@
+package com.ssafy.newjibs.house.controller;
+
+import com.ssafy.newjibs.house.service.HouseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/houses")
+public class HouseController {
+    private final HouseService houseService;
+
+}
