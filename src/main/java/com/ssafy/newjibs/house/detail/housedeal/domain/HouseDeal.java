fbdab40b8fbc9a9a25f65000fba933be3157548f
takealook <takealook97@naver.com>
2023-10-12 17:31:59 +0900

    feat: add HouseDeal entity class
    set N : 1 relationship from houseinfo.aptCode

---
+++ src/main/java/com/ssafy/newjibs/house/detail/housedeal/domain/HouseDeal.java
@@ -0,0 +1,32 @@
+package com.ssafy.newjibs.house.detail.housedeal.domain;
+
+import com.ssafy.newjibs.house.detail.houseinfo.domain.HouseInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "houseinfo")
+public class HouseDeal {
+    @Id
+    @Column(nullable = false)
+    private Long no;
+    @ManyToOne// N : 1 관계
+    @JoinColumn(name = "aptCode")
+    private HouseInfo houseInfo;
+    private String dealAmount;
+    private Integer dealYear;
+    private Integer dealMonth;
+    private Integer dealDay;
+    private String area;
+    private String floor;
+    private String cancelDealType;
+    private Integer aptCode;
+}
