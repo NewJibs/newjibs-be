4ed4d719da5ebe8c7acf50864fda085316d4b1c6
takealook97 <takealook97@naver.com>
2023-10-15 05:38:40 +0900

    feat: add LoginController class
    put sign in method

---
+++ src/main/java/com/ssafy/newjibs/member/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.ssafy.newjibs.member.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.newjibs.member.dto.LoginDto;
+import com.ssafy.newjibs.member.service.MemberService;
+import com.ssafy.newjibs.response.BaseResponse;
+import com.ssafy.newjibs.response.ResponseStatus;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class LoginController {
+	private final MemberService memberService;
+
+	@PostMapping("/login")
+	public BaseResponse<String> login(@RequestBody LoginDto loginDto) {
+		String token = memberService.login(loginDto);
+		return new BaseResponse<>(ResponseStatus.SUCCESS, token);
+	}
+}
