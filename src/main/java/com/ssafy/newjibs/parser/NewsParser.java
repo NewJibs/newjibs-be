13be6984b98f3ed46aea62277d2498ba9eee6ab3
takealook97 <takealook97@naver.com>
2023-10-14 17:34:15 +0900

    refactor: change method name to be intuitive and modified html parsing logic
    parsing content is performed by parseNewsContent method.
    also, add several image and description extraction logics to save those.
    add line length limit to 30 to extract writer's name and quit parsing generally.

--- src/main/java/com/ssafy/newjibs/parser/NewsParser.java
+++ src/main/java/com/ssafy/newjibs/parser/NewsParser.java
@@ -1,73 +1,101 @@
 package com.ssafy.newjibs.parser;
 
-import com.ssafy.newjibs.news.dto.NewsContentDto;
-import com.ssafy.newjibs.news.dto.NewsTitleDto;
-import com.ssafy.newjibs.parser.util.JsonMapper;
-import lombok.RequiredArgsConstructor;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.newjibs.news.dto.NewsContentDto;
+import com.ssafy.newjibs.news.dto.NewsTitleDto;
+import com.ssafy.newjibs.parser.dto.ContentParsingDto;
+import com.ssafy.newjibs.parser.util.JsonMapper;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class NewsParser {
-    private final JsonMapper jsonMapper;
+	private final JsonMapper jsonMapper;
+	final int WRITER_LENGTH_LIMIT = 30;
 
-    public List<NewsTitleDto> parseNewsTitles(String script) {
-        String JSON_START = "pageInfo = ";
-        String JSON_END = "}\n";
+	public List<NewsTitleDto> parseJson(String script) {// parse title
+		String JSON_START = "pageInfo = ";
+		String JSON_END = "}\n";
 
-        // JSON data extraction
-        int start = script.indexOf(JSON_START) + JSON_START.length();
-        int end = script.indexOf(JSON_END, start) + 1;
-        String jsonText = script.substring(start, end).replace("=", ":");// js to json
+		// JSON data extraction
+		int start = script.indexOf(JSON_START) + JSON_START.length();
+		int end = script.indexOf(JSON_END, start) + 1;
+		String jsonText = script.substring(start, end).replace("=", ":");// set text js to json
 
-        JSONObject jsonObject = new JSONObject(jsonText);
-        List<NewsTitleDto> list = new ArrayList<>();
-        // JSON parse
-        try {
-            // field extraction
-            JSONArray newsList = jsonObject.getJSONArray("newsList");
-            for (int i = 0; i < newsList.length(); i++) {
-                list.add(jsonMapper.toNewsMainDto(newsList.getJSONObject(i)));
-            }
-        } catch (Exception e) {// exception
-            e.printStackTrace();
-        }
-        return list;
-    }
+		JSONObject jsonObject = new JSONObject(jsonText);
+		List<NewsTitleDto> list = new ArrayList<>();
+		// parse JSON
+		try {
+			// field extraction
+			JSONArray newsList = jsonObject.getJSONArray("newsList");
+			for (int i = 0; i < newsList.length(); i++) {
+				list.add(jsonMapper.toNewsMainDto(newsList.getJSONObject(i)));
+			}
+		} catch (Exception e) {// exception
+			e.printStackTrace();
+		}
+		return list;
+	}
 
-    public NewsContentDto parseNewsContent(String title, String content, NewsTitleDto newsTitleDto) {
-        String writer = "";
-        content = content.replaceAll("<br>\\s*<br>", "<br>");// 개행 한 줄로 축소
+	public NewsContentDto parseHtml(NewsTitleDto newsTitleDto, Document document) {// parse content
+		Document contentDocument = Jsoup.parse(document.select("#article_body").html());// extract body from document
+		ContentParsingDto contentParsingDto = parseNewsContent(contentDocument.html());// it contains content and writer
 
-        StringBuilder parseStr = new StringBuilder();
-        String[] lines = content.split("<br>");
-        for (String line : lines) {// todo: kmp
-            if (line.contains("end_photo_org")) {// 줄에 img 관련 클래스가 있는 경우 스킵
-                continue;
-            }
+		return NewsContentDto.builder()
+			// title dto already involves these fields
+			.articleId(newsTitleDto.getArticleId())
+			.datetime(newsTitleDto.getDatetime())
+			.dayOfTheWeek(newsTitleDto.getDayOfTheWeek())
+			.officeHname(newsTitleDto.getOfficeHname())
+			// extract from css query
+			.contentTitle(document.select(".news_head_title").text())// title extraction through class
+			.imageUrl(contentDocument.select("img[src]").attr("src"))// image extraction through class
+			.imageDesc(contentDocument.select(".img_desc").text())// image description extraction through class
+			// extract from content parser
+			.content(contentParsingDto.getContent())
+			.writer(contentParsingDto.getWriter())
+			.build();
+	}
 
-            if (line.contains("기자") && line.contains("@")) {
-                writer = line;
-                break;
-            }
-            parseStr.append(line);
-        }
+	public ContentParsingDto parseNewsContent(String contentHtml) {
+		String writer = "";
+		String content = contentHtml.replaceAll("<br>\\s*<br>", "<br>");// shorten <br> tags
 
-        // 나머지 HTML 태그를 제거
-        content = parseStr.toString().replaceAll("<[^>]+>", "");
-        return NewsContentDto.builder()
-                .articleId(newsTitleDto.getArticleId())
-                .datetime(newsTitleDto.getDatetime())
-                .dayOfTheWeek(newsTitleDto.getDayOfTheWeek())
-                .officeHname(newsTitleDto.getOfficeHname())
-                .contentTitle(title)
-                .content(content)
-                .writer(writer)
-                .build();
-    }
+		StringBuilder parsedStr = new StringBuilder();
+		String[] lines = content.split("<br>");
+
+		// build content text and extract image, imageDesc and writer
+		for (String line : lines) {// todo: kmp
+			// image section
+			if (line.contains("img src=\"")) {// extract image
+				line = line.replaceAll("<em[^>]*>[^<]*</em>", "");// delete image and desc
+				parsedStr.append(line);
+				continue;
+			}
+
+			// writer line
+			if (line.contains("기자") && (line.contains("@") || line.length() <= WRITER_LENGTH_LIMIT)) {
+				writer = line;
+				break;
+			}
+			parsedStr.append(line);
+		}
+
+		content = parsedStr.toString().replaceAll("<[^>]+>", "");// delete rest of the html tags
+		content = content.replaceAll("(?:(\\r?\\n)\\s*){2,}", "$1"); // shorten crlf to once
+
+		return ContentParsingDto.builder()
+			.content(content)
+			.writer(writer)
+			.build();
+	}
 }
