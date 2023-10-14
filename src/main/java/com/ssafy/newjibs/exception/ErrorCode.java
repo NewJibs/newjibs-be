1f132a6ea756e59556c7bda672fa19e4dc893cbb
takealook97 <takealook97@naver.com>
2023-10-14 18:09:38 +0900

    feat: add 409 conflict error code on enum
    add some annotation to checking error codes to be added

--- src/main/java/com/ssafy/newjibs/exception/ErrorCode.java
+++ src/main/java/com/ssafy/newjibs/exception/ErrorCode.java
@@ -6,21 +6,35 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-    // 4xx: Client Errors
-    BAD_REQUEST(400, "Bad Request"),
-    UNAUTHORIZED(401, "Unauthorized"),
-    FORBIDDEN(403, "Forbidden"),
-    NOT_FOUND(404, "Not Found"),
-    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+	// member error code
 
-    // 5xx: Server Errors
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
-    NOT_IMPLEMENTED(501, "Not Implemented"),
-    BAD_GATEWAY(502, "Bad Gateway"),
-    SERVICE_UNAVAILABLE(503, "Service Unavailable"),
-    GATEWAY_TIMEOUT(504, "Gateway Timeout"),
-    HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
+	// house error code
 
-    private final Integer errorCode;
-    private final String errorMsg;
+	// news error code
+
+	// notice error code
+
+	// crawler error code
+
+	// parser error code
+
+	// === GLOBAL BASE ERROR CODE ===
+	// 4xx: Client Errors
+	BAD_REQUEST(400, "Bad Request"),
+	UNAUTHORIZED(401, "Unauthorized"),
+	FORBIDDEN(403, "Forbidden"),
+	NOT_FOUND(404, "Not Found"),
+	METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+	CONFLICT(409, "Conflict"),
+
+	// 5xx: Server Errors
+	INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+	NOT_IMPLEMENTED(501, "Not Implemented"),
+	BAD_GATEWAY(502, "Bad Gateway"),
+	SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+	GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+	HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
+
+	private final Integer errorCode;
+	private final String errorMsg;
 }
