61742fdd68183e94035f87fb786fc5ebae8812a3
takealook97 <takealook97@naver.com>
2023-10-25 23:20:10 +0900

    remove: delete method on Member controller and service
    delete all of methods to rebuild Member auth setting

--- src/main/java/com/ssafy/newjibs/member/controller/LoginController.java
+++ src/main/java/com/ssafy/newjibs/member/controller/LoginController.java
@@ -1,14 +1,9 @@
 package com.ssafy.newjibs.member.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.newjibs.member.dto.LoginDto;
 import com.ssafy.newjibs.member.service.MemberService;
-import com.ssafy.newjibs.response.BaseResponse;
-import com.ssafy.newjibs.response.ResponseStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,9 +13,4 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
 	private final MemberService memberService;
 
-	@PostMapping("/login")
-	public BaseResponse<String> login(@RequestBody LoginDto loginDto) {
-		String token = memberService.login(loginDto);
-		return new BaseResponse<>(ResponseStatus.SUCCESS, token);
-	}
 }
--- src/main/java/com/ssafy/newjibs/member/controller/MemberController.java
+++ src/main/java/com/ssafy/newjibs/member/controller/MemberController.java
@@ -1,16 +1,9 @@
 package com.ssafy.newjibs.member.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.newjibs.member.dto.RegisterDto;
 import com.ssafy.newjibs.member.service.MemberService;
-import com.ssafy.newjibs.response.BaseResponse;
-import com.ssafy.newjibs.response.ResponseStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,8 +13,4 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	private final MemberService memberService;
 
-	@PostMapping("/signup")
-	public BaseResponse<Long> saveMember(@Valid @RequestBody RegisterDto registerDto) {
-		return new BaseResponse<>(ResponseStatus.SUCCESS, memberService.register(registerDto));
-	}
 }
