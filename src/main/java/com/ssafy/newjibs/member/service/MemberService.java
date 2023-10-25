ace5cd0006c9c02898ee6b441218cfc3aa297d01
takealook97 <takealook97@naver.com>
2023-10-26 02:41:51 +0900

    feat: add MemberService class
    put register method to save user. also, get method is for checking member's authorities which can be diverse

--- src/main/java/com/ssafy/newjibs/member/service/MemberService.java
+++ src/main/java/com/ssafy/newjibs/member/service/MemberService.java
@@ -1,10 +1,18 @@
 package com.ssafy.newjibs.member.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.newjibs.exception.BaseException;
+import com.ssafy.newjibs.exception.ErrorCode;
+import com.ssafy.newjibs.member.domain.Authority;
+import com.ssafy.newjibs.member.domain.Member;
+import com.ssafy.newjibs.member.dto.MemberDto;
+import com.ssafy.newjibs.member.dto.RegisterDto;
 import com.ssafy.newjibs.member.repository.MemberRepository;
 import com.ssafy.newjibs.member.util.MemberMapper;
+import com.ssafy.newjibs.member.util.SecurityUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +21,32 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberService {
 	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
 	private final MemberMapper memberMapper;
 
+	public MemberDto register(RegisterDto registerDto) {
+		if (memberRepository.existsByEmail(registerDto.getEmail())) {
+			throw new BaseException(ErrorCode.DUPLICATED_EMAIL);
+		}
+
+		Authority authority = Authority.builder()
+			.authorityName("ROLE_USER")
+			.build();
+
+		Member member = memberMapper.toEntity(registerDto, passwordEncoder.encode(registerDto.getPassword()), authority);
+
+		return memberMapper.toDto(memberRepository.save(member));
+	}
+
+	public MemberDto getMemberWithAuthorities(String email) {
+		return memberMapper.toDto(
+			memberRepository.findByEmail(email).orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND)));
+	}
+
+	public MemberDto getMyMemberWithAuthorities() {
+		return memberMapper.toDto(SecurityUtil.getCurrentEmail()
+			.flatMap(memberRepository::findOneWithAuthoritiesByEmail)
+			.orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND))
+		);
+	}
 }
