97bef19fadcc670527e5605c817d5aea5a8427fd
takealook97 <takealook97@naver.com>
2023-10-26 02:48:03 +0900

    feat: add MemberController class
    it check auth through PreAutorize annotaion. it's a sort of aspect thing to define member's role

--- src/main/java/com/ssafy/newjibs/member/controller/MemberController.java
+++ src/main/java/com/ssafy/newjibs/member/controller/MemberController.java
@@ -1,8 +1,19 @@
 package com.ssafy.newjibs.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.newjibs.member.dto.MemberDto;
+import com.ssafy.newjibs.member.dto.RegisterDto;
 import com.ssafy.newjibs.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,4 +24,26 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	private final MemberService memberService;
 
+	@PostMapping("/register")
+	public ResponseEntity<MemberDto> register(@Valid @RequestBody RegisterDto registerDto) {
+		return ResponseEntity.ok(memberService.register(registerDto));
+	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<Void> logout() {
+
+		return null;
+	}
+
+	@GetMapping("/member")
+	@PreAuthorize("hasAnyRole('USER','ADMIN')")
+	public ResponseEntity<MemberDto> getMyUserInfo(HttpServletRequest request) {
+		return ResponseEntity.ok(memberService.getMyMemberWithAuthorities());
+	}
+
+	@GetMapping("/member/{email}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<MemberDto> getUserInfo(@PathVariable String email) {
+		return ResponseEntity.ok(memberService.getMemberWithAuthorities(email));
+	}
 }
