cdbb1b8a95488033f4e231a491c8c7cc8ddd9b22
takealook97 <takealook97@naver.com>
2023-10-27 23:49:35 +0900

    refactor: change controller's return type BaseResponse to ResponseEntity
    because of the member controller, unifying return type would be better, so deleted BaseResponse and changed the type to ResponseEntity

--- src/main/java/com/ssafy/newjibs/news/controller/NewsController.java
+++ src/main/java/com/ssafy/newjibs/news/controller/NewsController.java
@@ -1,44 +1,47 @@
 package com.ssafy.newjibs.news.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ssafy.newjibs.crawler.WebCrawler;
 import com.ssafy.newjibs.news.dto.NewsContentDto;
 import com.ssafy.newjibs.news.dto.NewsTitleDto;
 import com.ssafy.newjibs.news.service.NewsService;
-import com.ssafy.newjibs.response.BaseResponse;
-import com.ssafy.newjibs.response.ResponseStatus;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/news")
 public class NewsController {
-    private final NewsService newsService;
-    private final WebCrawler webCrawler;
+	private final NewsService newsService;
+	private final WebCrawler webCrawler;
 
-    @GetMapping("/test/crawling")
-    public BaseResponse<Void> crawlNews() {
-        webCrawler.crawlAndSaveNews();
-        return new BaseResponse<>(ResponseStatus.SUCCESS);
-    }
+	@GetMapping("/test/crawling")
+	public ResponseEntity<Void> crawlNews() {
+		webCrawler.crawlAndSaveNews();
+		return ResponseEntity.ok().build();
+	}
 
-    @GetMapping
-    public BaseResponse<List<NewsTitleDto>> getAllNewsTitles() {
-        List<NewsTitleDto> list = newsService.readAllNews();
-        return new BaseResponse<>(ResponseStatus.SUCCESS, list);
-    }
+	@GetMapping
+	public ResponseEntity<List<NewsTitleDto>> getAllNewsTitles() {
+		return ResponseEntity.ok(newsService.readAllNews());
+	}
 
-    @GetMapping("/{articleId}")
-    public BaseResponse<NewsContentDto> getNewsContent(@PathVariable String articleId) {
-        NewsContentDto newsContentDto = newsService.readNews(articleId);
-        return new BaseResponse<>(ResponseStatus.SUCCESS, newsContentDto);
-    }
+	@GetMapping("/{articleId}")
+	public ResponseEntity<NewsContentDto> getNewsContent(@PathVariable String articleId) {
+		return ResponseEntity.ok(newsService.readNews(articleId));
+	}
 
-    @DeleteMapping("/{articleId}")
-    public BaseResponse<Void> removeNews(@PathVariable String articleId) {
-        newsService.deleteNews(articleId);
-        return new BaseResponse<>(ResponseStatus.SUCCESS);
-    }
+	@DeleteMapping("/{articleId}")
+	public ResponseEntity<Void> removeNews(@PathVariable String articleId) {
+		newsService.deleteNews(articleId);
+		return ResponseEntity.ok().build();
+	}
 }
--- src/main/java/com/ssafy/newjibs/notice/controller/NoticeController.java
+++ src/main/java/com/ssafy/newjibs/notice/controller/NoticeController.java
@@ -1,48 +1,54 @@
 package com.ssafy.newjibs.notice.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ssafy.newjibs.notice.dto.NoticeDto;
 import com.ssafy.newjibs.notice.dto.NoticeListDto;
 import com.ssafy.newjibs.notice.service.NoticeService;
-import com.ssafy.newjibs.response.BaseResponse;
-import com.ssafy.newjibs.response.ResponseStatus;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/notices")
 public class NoticeController {
-    private final NoticeService noticeService;
+	private final NoticeService noticeService;
 
-    @PostMapping
-    public BaseResponse<Void> saveNotice(@RequestBody NoticeDto noticeDto) {
-        noticeService.createNotice(noticeDto);
-        return new BaseResponse<>(ResponseStatus.CREATED);
-    }
+	@PostMapping
+	public ResponseEntity<Void> saveNotice(@RequestBody NoticeDto noticeDto) {
+		noticeService.createNotice(noticeDto);
+		return ResponseEntity.ok().build();
+	}
 
-    @GetMapping("/{noticeId}")
-    public BaseResponse<NoticeDto> getNotice(@PathVariable Long noticeId) {
-        NoticeDto noticeDto = noticeService.readNotice(noticeId);
-        return new BaseResponse<>(ResponseStatus.SUCCESS, noticeDto);
-    }
+	@GetMapping("/{noticeId}")
+	public ResponseEntity<NoticeDto> getNotice(@PathVariable Long noticeId) {
+		return ResponseEntity.ok(noticeService.readNotice(noticeId));
+	}
 
-    @GetMapping
-    public BaseResponse<List<NoticeListDto>> getAllNotice() {
-        List<NoticeListDto> list = noticeService.readAllNotice();
-        return new BaseResponse<>(ResponseStatus.SUCCESS, list);
-    }
+	@GetMapping
+	public ResponseEntity<List<NoticeListDto>> getAllNotice() {
+		return ResponseEntity.ok(noticeService.readAllNotice());
+	}
 
-    @PatchMapping("/{noticeId}")
-    public BaseResponse<Void> modifyNotice(@PathVariable Long noticeId, @RequestBody NoticeDto noticeDto) {
-        noticeService.updateNotice(noticeId, noticeDto);
-        return new BaseResponse<>(ResponseStatus.SUCCESS);
-    }
+	@PatchMapping("/{noticeId}")
+	public ResponseEntity<Void> modifyNotice(@PathVariable Long noticeId, @RequestBody NoticeDto noticeDto) {
+		noticeService.updateNotice(noticeId, noticeDto);
+		return ResponseEntity.ok().build();
+	}
 
-    @DeleteMapping("/{noticeId}")
-    public BaseResponse<Void> removeNotice(@PathVariable Long noticeId) {
-        noticeService.deleteNotice(noticeId);
-        return new BaseResponse<>(ResponseStatus.SUCCESS);
-    }
+	@DeleteMapping("/{noticeId}")
+	public ResponseEntity<Void> removeNotice(@PathVariable Long noticeId) {
+		noticeService.deleteNotice(noticeId);
+		return ResponseEntity.ok().build();
+	}
 }
