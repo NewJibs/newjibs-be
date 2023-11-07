7e8f656acea1b9c13742d5e4f81a9563c57a4dad
takealook <takealook97@naver.com>
2023-11-07 10:06:29 +0900

    feat: enable scheduling

--- src/main/java/com/ssafy/newjibs/NewJibsApplication.java
+++ src/main/java/com/ssafy/newjibs/NewJibsApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.newjibs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewJibsApplication {
 
 	public static void main(String[] args) {
