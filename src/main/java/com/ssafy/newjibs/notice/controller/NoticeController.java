edbc71bd48bd9a7220c92e641638d2228f59732e
takealook <takealook97@naver.com>
2023-11-10 10:36:57 +0900

    fix: set modifying http method to put
    using wildcard to permit all of http methods, but exceptionally "patch" method was not allowed.(maybe the reason is server's proxy or api gateway)

--- src/main/java/com/ssafy/newjibs/notice/controller/NoticeController.java
+++ src/main/java/com/ssafy/newjibs/notice/controller/NoticeController.java
@@ -1,51 +1,60 @@
 package com.ssafy.newjibs.notice.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ssafy.newjibs.notice.dto.NoticeDto;
 import com.ssafy.newjibs.notice.dto.NoticeListDto;
 import com.ssafy.newjibs.notice.service.NoticeService;
+
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/notices")
 public class NoticeController {
-    private final NoticeService noticeService;
+	private final NoticeService noticeService;
 
-    @ApiOperation(value = "공지사항을 저장한다.")
-    @PostMapping
-    public ResponseEntity<Void> saveNotice(@RequestBody NoticeDto noticeDto) {
-        noticeService.createNotice(noticeDto);
-        return ResponseEntity.ok().build();
-    }
+	@ApiOperation(value = "공지사항을 저장한다.")
+	@PostMapping
+	public ResponseEntity<Void> saveNotice(@RequestBody NoticeDto noticeDto) {
+		noticeService.createNotice(noticeDto);
+		return ResponseEntity.ok().build();
+	}
 
-    @ApiOperation(value = "특정 공지사항을 읽어온다.")
-    @GetMapping("/{noticeId}")
-    public ResponseEntity<NoticeDto> getNotice(@PathVariable Long noticeId) {
-        return ResponseEntity.ok(noticeService.readNotice(noticeId));
-    }
+	@ApiOperation(value = "특정 공지사항을 읽어온다.")
+	@GetMapping("/{noticeId}")
+	public ResponseEntity<NoticeDto> getNotice(@PathVariable Long noticeId) {
+		return ResponseEntity.ok(noticeService.readNotice(noticeId));
+	}
 
-    @ApiOperation(value = "모든 공지사항을 읽어온다.")
-    @GetMapping
-    public ResponseEntity<List<NoticeListDto>> getAllNotice() {
-        return ResponseEntity.ok(noticeService.readAllNotice());
-    }
+	@ApiOperation(value = "모든 공지사항을 읽어온다.")
+	@GetMapping
+	public ResponseEntity<List<NoticeListDto>> getAllNotice() {
+		return ResponseEntity.ok(noticeService.readAllNotice());
+	}
 
-    @ApiOperation(value = "공지사항을 수정한다.")
-    @PatchMapping("/{noticeId}")
-    public ResponseEntity<Void> modifyNotice(@PathVariable Long noticeId, @RequestBody NoticeDto noticeDto) {
-        noticeService.updateNotice(noticeId, noticeDto);
-        return ResponseEntity.ok().build();
-    }
+	@ApiOperation(value = "공지사항을 수정한다.")
+	@PutMapping("/{noticeId}")
+	public ResponseEntity<Void> modifyNotice(@PathVariable Long noticeId, @RequestBody NoticeDto noticeDto) {
+		noticeService.updateNotice(noticeId, noticeDto);
+		return ResponseEntity.ok().build();
+	}
 
-    @ApiOperation(value = "공지사항을 삭제한다.")
-    @DeleteMapping("/{noticeId}")
-    public ResponseEntity<Void> removeNotice(@PathVariable Long noticeId) {
-        noticeService.deleteNotice(noticeId);
-        return ResponseEntity.ok().build();
-    }
+	@ApiOperation(value = "공지사항을 삭제한다.")
+	@DeleteMapping("/{noticeId}")
+	public ResponseEntity<Void> removeNotice(@PathVariable Long noticeId) {
+		noticeService.deleteNotice(noticeId);
+		return ResponseEntity.ok().build();
+	}
 }
