49d10b2bce2b097f98b185dddf8ceb7e22c679eb
takealook <takealook97@naver.com>
2023-11-19 05:58:25 +0900

    feat: add repository setting on config file

--- src/main/java/com/ssafy/newjibs/config/RedisConfig.java
+++ src/main/java/com/ssafy/newjibs/config/RedisConfig.java
@@ -2,11 +2,30 @@ package com.ssafy.newjibs.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import com.ssafy.newjibs.member.repository.MemberRepository;
+import com.ssafy.newjibs.member.repository.TokenRedisRepository;
 
 @Configuration
+@EnableJpaRepositories(
+	includeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = {MemberRepository.class}
+	)
+)
+@EnableRedisRepositories(
+	includeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = {TokenRedisRepository.class}
+	)
+)
 public class RedisConfig {
 
 	@Value("${spring.redis.host}")
@@ -19,4 +38,3 @@ public class RedisConfig {
 		return new LettuceConnectionFactory(host, port);
 	}
 }
-
