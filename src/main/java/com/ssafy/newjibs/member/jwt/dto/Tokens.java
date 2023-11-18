7cd19d2a74097a81b13b2f7d79177e0dc69f4d52
takealook <takealook97@naver.com>
2023-11-19 03:52:11 +0900

    style: rename token dto to tokens

--- src/main/java/com/ssafy/newjibs/member/jwt/dto/Tokens.java
+++ src/main/java/com/ssafy/newjibs/member/jwt/dto/Tokens.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TokenDto {
+public class Tokens {
 	private String accessToken;
 	private String refreshToken;
 }
