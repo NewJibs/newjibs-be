d9f8db4524c60b9af8e6bb0408764b5a6cb2413b
takealook97 <takealook97@naver.com>
2023-11-20 21:49:46 +0900

    feat: add RankDto to show rank info

---
+++ src/main/java/com/ssafy/newjibs/member/dto/RankDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.newjibs.member.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RankDto {
+	@JsonProperty("email")
+	private String email;
+
+	@JsonProperty("point")
+	private Long point;
+}
