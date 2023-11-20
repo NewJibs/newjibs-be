b19302598286879204fd9f418fef6059c2a4bd87
takealook <takealook97@naver.com>
2023-11-20 17:36:23 +0900

    feat: add result method on house controller layer

--- src/main/java/com/ssafy/newjibs/house/controller/HouseController.java
+++ src/main/java/com/ssafy/newjibs/house/controller/HouseController.java
@@ -1,15 +1,18 @@
 package com.ssafy.newjibs.house.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.newjibs.house.dto.Coordinate;
 import com.ssafy.newjibs.house.dto.HouseDto;
+import com.ssafy.newjibs.house.dto.HouseResultDto;
 import com.ssafy.newjibs.house.service.HouseService;
 
 import lombok.RequiredArgsConstructor;
@@ -28,5 +31,10 @@ public class HouseController {
 	@GetMapping("/{aptCode}")
 	public ResponseEntity<List<HouseDto>> getHouses(@PathVariable Long aptCode) {
 		return ResponseEntity.ok(houseService.findHouseDtosByAptCode(aptCode));
+	}
+
+	@GetMapping("/results")
+	public ResponseEntity<Map<Long, HouseResultDto>> getResults(@RequestBody List<Long> nos) {
+		return ResponseEntity.ok(houseService.findHouseResultDtoForGiven2020DealNo(nos));
 	}
 }
